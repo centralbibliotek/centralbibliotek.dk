a7cc9dd53df4d40c9eefd8e9ace014d1ad35d257
Arne Jørgensen <arne@arnested.dk>
2014-09-09 12:56:49 +0200

    Save our "custom" files between situs builds.
    "Custom" is everything that is not part of the Drupal Commons make file.
    
    This way we can rebuild the Drupal Commons part and still keep
    everything in a OMG repository.

---
+++ sites/all/drush/drushrc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Centralbibliotek.dk specific drush configuration.
+ */
+
+/**
+ * Save our "custom" files between situs builds.
+ *
+ * "Custom" is everything that is not part of the Drupal Commons make file.
+ */
+$command_specific = array(
+  'situs' => array(
+    'save-files' => array(
+      '.git',
+      '.scrutinizer.yml',
+      'circle.yml',
+      'sites/all/drush',
+      'sites/all/modules',
+    ),
+  ),
+);
