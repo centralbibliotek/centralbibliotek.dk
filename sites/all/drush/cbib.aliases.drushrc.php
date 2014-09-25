2280449ecabc14a1cb26a5b08035dcad7e9c0acd
Arne Jørgensen <arne@arnested.dk>
2014-09-25 10:50:07 +0200

    Added Deployotron 1.2 support.

---
+++ sites/all/drush/cbib.aliases.drushrc.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Drush aliases for centralbibliotek.dk.
+ */
+
+$aliases['test'] = array(
+  'uri' => 'default',
+  'root' => '/var/www/centralbibliotek.dk',
+  'remote-host' => 'test1404.reload.dk',
+  'remote-user' => 'reload',
+  'path-aliases' => array(
+    '%dump' => '~/cbib_test.sql',
+  ),
+  'deployotron' => array(
+    'branch' => 'develop',
+    'restart-apache2' => TRUE,
+    'restart-varnish' => FALSE,
+    // 'no-updb' => TRUE,
+    // 'message' => 'Remember to run drush @stage updb manually',
+    // 'newrelic-api-key' => '0d8954eb45f4f9753b64df3bcd559f9b0ac8b81ef9c4ff3',
+    // 'newrelic-app-name' => 'coopanalyse.dk-stage',
+  ),
+);
