944d9118fe313f33c0560a8b6f8e87891f8e0026
Ying Ho <ying@openconcept.dk>
2015-01-10 15:57:48 +0100

    CBDK-27: All teaser node changes so centralbibliotek and topics is under the image. And the whole teaser node is now a link. Styling of the teaser node.

--- profiles/commons/themes/cb/scripts/cb.js
+++ profiles/commons/themes/cb/scripts/cb.js
@@ -1,17 +1,55 @@
 // Misc javascript for the cb theme
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
 
-// Add a placeholder string to the search field (in the header)
-jQuery(document).ready(function($) {
-  $('#search-block-form input[type="search"]').attr('placeholder', 'Søg på sitet...');
-});
+  // JavaScript should be made compatible with libraries other than jQuery by
+  // wrapping it with an "anonymous closure". See:
+  // - https://drupal.org/node/1446420
+  // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
 
 
-// Move the Commons utility links to another html element.
-// This is just a quick fix and should be refactored the Drupal way as the project matures.
-jQuery(document).ready(function($) {
-  var source = $("#block-commons-utility-links-commons-utility-links").contents();
-  var target = $('#main-menu .utility-links');
+  // Add a placeholder string to the search field (in the header).
+  Drupal.behaviors.searchPlaceholder = {
+    attach: function (context, settings) {
+      $('#search-block-form input[type="search"]').attr('placeholder', 'Søg på sitet...');
+    }
+  }
 
-  $(target).append(source);
-});
+  // Move the Commons utility links to another html element.
+  // This is just a quick fix and should be refactored the Drupal way as the project matures.
+  Drupal.behaviors.replaceCommonsUtility = {
+    attach: function (context, settings) {
+      var source = $("#block-commons-utility-links-commons-utility-links").contents();
+      var target = $('#main-menu .utility-links');
 
+      $(target).append(source);
+    }
+  }
+
+  // Add link to whole teaser node.
+  Drupal.behaviors.addLinkToElement = {
+    attach: function (context, settings) {
+      // Add hover class to element with class 'clickable' only if there's a link inside.
+      $('.node-teaser .center-wrapper .panel-panel', context).hover(function () {
+        $(this).toggleClass('hover');
+      });
+
+      // Add click event to whole block that holds a link.
+      $(document, context).on('click', '.node-teaser .center-wrapper .panel-panel', function (event) {
+        var href = $(this).find("a").attr("href");
+        if (typeof href === "undefined") {
+          href = $(this).parent().find("a").attr("href");
+        }
+        window.location = href;
+        return false;
+      });
+    }
+  }
+
+})(jQuery, Drupal, this, this.document);
