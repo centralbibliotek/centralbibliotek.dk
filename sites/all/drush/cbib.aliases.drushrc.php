0cba532358b98491be1da4c3e8bdf0a8b7206497
Arne Jørgensen <arne@arnested.dk>
2015-02-03 13:54:44 +0100

    Added production server to deployotron config.

--- sites/all/drush/cbib.aliases.drushrc.php
+++ sites/all/drush/cbib.aliases.drushrc.php
@@ -7,6 +7,23 @@
 
 putenv('CBIB_DRUSH_DIR=' . __DIR__);
 
+$aliases['prod'] = array(
+  'uri' => 'ny.centralbibliotek.dk',
+  // 'root' => '/var/www/centralbibliotek.dk',
+  'remote-host' => 'ny.centralbibliotek.dk',
+  'remote-user' => 'reload',
+  'ssh-options' => '-F ' . __DIR__ . '/ssh/config -o GlobalKnownHostsFile=' . __DIR__ . '/ssh/known_hosts',
+  'path-aliases' => array(
+    '%drush-script' => '/home/reload/bin/drush',
+  ),
+  'deployotron' => array(
+    'branch' => 'develop',
+    'restart-apache2' => FALSE,
+    'restart-varnish' => FALSE,
+    'flowdock-token' => 'a7200e7e654379df303056fbf14faf74',
+  ),
+);
+
 $aliases['test'] = array(
   'uri' => 'cbib.test1404.reload.dk',
   'root' => '/var/www/centralbibliotek.dk',
