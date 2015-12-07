667f8dc09a8a79f118020729c3adf5b974603b12
Arne Jørgensen <arne@arnested.dk>
2015-12-07 10:20:51 +0100

    Remove Reload keys from Deployotron.

--- sites/all/drush/cbib.aliases.drushrc.php
+++ sites/all/drush/cbib.aliases.drushrc.php
@@ -12,7 +12,6 @@ $aliases['prod'] = array(
   'root' => '/data/www/centralbibliotek.dk',
   'remote-host' => 'centralbib.dbc.dk',
   'remote-user' => 'reload',
-  'ssh-options' => '-F ' . __DIR__ . '/ssh/config -o GlobalKnownHostsFile=' . __DIR__ . '/ssh/known_hosts -o LogLevel=Error',
   'dump-dir' => '/home/reload/backups',
   'path-aliases' => array(
     '%drush-script' => '/home/reload/bin/drush',
@@ -23,9 +22,6 @@ $aliases['prod'] = array(
     'num-dumps' => 3,
     'restart-apache2' => TRUE,
     'restart-varnish' => FALSE,
-    'flowdock-token' => 'a7200e7e654379df303056fbf14faf74',
-    'newrelic-api-key' => '120fd65c787d6de27897a44cce59495fa4002740d8e367c',
-    'newrelic-app-name' => 'centralbibliotek.dk',
   ),
   // Skip cache tables and the like.
   'source-command-specific' => array (
@@ -44,23 +40,6 @@ $aliases['prod'] = array(
         ),
       ),
     ),
-  ),
-);
-
-$aliases['test'] = array(
-  'uri' => 'cbib.test1404.reload.dk',
-  'root' => '/var/www/centralbibliotek.dk',
-  'remote-host' => 'test1404.reload.dk',
-  'remote-user' => 'reload',
-  'path-aliases' => array(
-    '%drush-script' => '/home/reload/bin/drush',
-  ),
-  'dump-dir' => '/home/reload/backups',
-  'deployotron' => array(
-    'branch' => 'develop',
-    'restart-apache2' => FALSE,
-    'restart-varnish' => FALSE,
-    'flowdock-token' => 'a7200e7e654379df303056fbf14faf74',
   ),
 );
 
