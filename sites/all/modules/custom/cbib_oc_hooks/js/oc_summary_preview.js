6199eb8fa3e1e20384271ff2f5ef9564f89c7216
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-02 15:36:51 +0100

    hardcode teaser length to fit. added pre-load teaser placeholder for existing nodes.

--- sites/all/modules/custom/cbib_oc_hooks/js/oc_summary_preview.js
+++ sites/all/modules/custom/cbib_oc_hooks/js/oc_summary_preview.js
@@ -8,7 +8,8 @@
     
     $( document ).ready(function() {
         var editor = CKEDITOR.instances['edit-body-und-0-value'];
-        $('textarea[id^=edit-body-und-0-summary]').attr('placeholder',editor.getData());
+        $('textarea[id^=edit-body-und-0-summary]').attr('placeholder','Loading..');
+        initSummary();
         
         editor.on( 'contentDom', function() {
             var editable = editor.editable();
@@ -50,6 +51,45 @@
                      });
                  }
             });
+            editor.fire( 'blur' );
         });
+        function initSummary()
+        {
+             var editor = CKEDITOR.instances['edit-body-und-0-value'];
+             var data = editor.getData();
+             if(data.length > 0)
+             {
+                 var throbber = $('<div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div>');
+                 var label = $('label[for^=edit-body-und-0-summary]');
+                 
+                label.prepend(throbber);
+                jQuery.ajax({
+                 method: "POST",
+                 url: "/admin/settings/cbib_oc_hooks/js/summary",
+                 data: { text: data},
+                 success: function(response) { 
+                     debugger;
+                     /*
+                      * Set placeholder on hidden text area so it is restored
+                      * after clear events.
+                      */
+                     var textarea = $('textarea[id^=edit-body-und-0-summary]');
+                     textarea.attr('placeholder',response);
+
+                     /*
+                      * Set the iframe placeholder content to the new
+                      * placeholder
+                      */
+                     var iframe = $("#cke_edit-body-und-0-summary").find('.cke_wysiwyg_frame');
+                     var test2 = iframe.contents().find('.placeholder').html(response);
+                     /*
+                      * Remove spinner
+                      */
+                     var label = $('label[for^=edit-body-und-0-summary]');
+                     label.find('.ajax-progress').remove();
+                     }
+               });
+             }
+        }
     });
 })(jQuery);
