858cdef50f1493bf5fd14d8d9d71755c3a38f5ed
Tobias F. Petersen <tobsf@hotmail.com>
2017-11-23 15:00:38 +0100

    iphone mail is fucked

--- sites/all/themes/cb/templates/mimemail-message.tpl.php
+++ sites/all/themes/cb/templates/mimemail-message.tpl.php
@@ -32,7 +32,6 @@
       }
       -->
     </style>
-    
   </head>
   <body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?>>
     <div id="center">
@@ -40,7 +39,7 @@
         <div style="width:100%;background-color:#333333;">
             <table style="width:100%;background-color:#333333;font-weight:700;font-size:22px;">
                 <tbody>
-                <td style="width:100%;padding:10px;"><a href="http://centralbibliotek.dk" class="mail-site-logo" style="color:#ff5479;text-decoration: none;">Centralbibliotek.dk</a></td>
+                <td style="width:100%;padding:10px;"><span class="mail-site-logo" style="color:#ff5479;font-size:22px;">Centralbibliotek.dk</span></td>
                 </tbody>
             </table>
         </div>
