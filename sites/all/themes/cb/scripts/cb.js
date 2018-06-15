8665e78cdf6b45e38df12be768de71e3d4e6a1e3
impara <impara1@gmail.com>
2018-06-15 12:53:26 +0200

    IE fix for group facet style on global search

--- sites/all/themes/cb/scripts/cb.js
+++ sites/all/themes/cb/scripts/cb.js
@@ -13,6 +13,36 @@
 (function ($, Drupal, window, document) {
 
   "use strict";
+  
+  /**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // IE 12 => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
 
     function trigger_loginSpinner() {
         
@@ -98,9 +128,11 @@
                     $('.fix_me_select').val(e.currentTarget.value);
                      return false;
          });         
-
+if(!detectIE()) {
         $('#facetapi-facet-search-apiglobal-search-block-nodeog-group-ref').parent().parent().parent().appendTo(".view-filters #views-exposed-form-search-api-nodes-default").css('position','relative').css('left','60%').css('bottom','65px');
-
+        }else {
+        $('#facetapi-facet-search-apiglobal-search-block-nodeog-group-ref').parent().parent().parent().appendTo(".view-filters #views-exposed-form-search-api-nodes-default").css('position','relative').css('left','60%').css('bottom','70px');
+        }
 
     $('#facetapi-facet-search-apiglobal-search-block-nodeog-group-ref #edit-facets--5 > option').css('width','95px');         
                
