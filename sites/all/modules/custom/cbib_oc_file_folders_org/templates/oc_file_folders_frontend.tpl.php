9bc4d1a7fe9bae55c630c70fb25da3c7379a8925
Tobias F. Petersen <tobsf@hotmail.com>
2018-07-16 14:34:20 +0200

    Commit cbib_oc_file_folders_org module working. needs theming

---
+++ sites/all/modules/custom/cbib_oc_file_folders_org/templates/oc_file_folders_frontend.tpl.php
@@ -0,0 +1,28 @@
+<div class="oc_file_folder_wrapper">
+<?php 
+echo "<div id='0'>";
+foreach($items as $key => $item)
+{
+    if($item['data']['#default_value']['is_folder'])
+    {
+        $class = 'oc-is-folder file';
+        $folder_name = isset($item['data']['#file']['description']) ? $item['data']['#file']['description'] : $item['data']['name'];
+        echo "<div class='".$class."'><a class='' id='".$item['data']['mlid']['#value']."' >" .$folder_name ."</a></div>";
+    }
+    else
+    {
+        $file = file_load($item['data']['#file']['fid']);
+        if(!empty( $item['data']['#file']['description']))
+        {
+            $file->description = $item['data']['#file']['description'];
+        }
+         echo "<div>" .  theme_file_link(array('file' => $file)) . "</div>";
+    }
+}
+echo "</div>";
+foreach($items as $key => $item)
+{
+    echo oc_file_folders_render_children_frontend($item['children'],$item['data']['mlid']['#value']);
+}
+?>
+</div>
