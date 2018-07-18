99066edbed04a68e2e1641f86ae5ea383065c4df
Tobias F. Petersen <tobsf@hotmail.com>
2018-07-18 09:40:33 +0200

    weigth sort now works + other fix and refactorings and some cleaning

--- sites/all/modules/custom/cbib_oc_file_folders_org/templates/oc_file_folders_frontend.tpl.php
+++ sites/all/modules/custom/cbib_oc_file_folders_org/templates/oc_file_folders_frontend.tpl.php
@@ -1,28 +1,21 @@
 <div class="oc_file_folder_wrapper">
-<?php 
-echo "<div id='0'>";
-foreach($items as $key => $item)
-{
-    if($item['data']['#default_value']['is_folder'])
-    {
-        $class = 'oc-is-folder file';
-        $folder_name = isset($item['data']['#file']['description']) ? $item['data']['#file']['description'] : $item['data']['name'];
-        echo "<div class='".$class."'><img style='margin-right:3px' src='/".drupal_get_path('module', 'cbib_oc_file_folders_org') . "/images/folder.png"."'><a class='' id='".$item['data']['mlid']['#value']."' >" .$folder_name ."</a></div>";
-    }
-    else
-    {
-        $file = file_load($item['data']['#file']['fid']);
-        if(!empty( $item['data']['#file']['description']))
-        {
-            $file->description = $item['data']['#file']['description'];
+    <?php
+    echo "<div id='0'>";
+    foreach ($items as $key => $item) {
+        if ($item['data']['#default_value']['is_folder']) {
+            echo theme("file_oc_file_folder", array('row' => $item));
+        } else {
+            $file = file_load($item['data']['#file']['fid']);
+            //the data is sometimes diff...perhaps try to normalize.
+            if (!empty($item['data']['#file']['description']) || !empty($item['data']['description']['#value'])) {
+                $file->description = !empty($item['data']['#file']['description']) ? $item['data']['#file']['description'] : $item['data']['description']['#value'];
+            }
+            echo "<div>" . theme_file_link(array('file' => $file)) . "</div>";
         }
-         echo "<div>" .  theme_file_link(array('file' => $file)) . "</div>";
     }
-}
-echo "</div>";
-foreach($items as $key => $item)
-{
-    echo oc_file_folders_render_children_frontend($item['children'],$item['data']['mlid']['#value']);
-}
-?>
+    echo "</div>";
+    foreach ($items as $key => $item) {
+        echo oc_file_folders_render_children_frontend($item['children'], $item['data']['mlid']['#value']);
+    }
+    ?>
 </div>
