665b14c04bb51792c0829e1de70fb77b2f3ef31c
Tobias F. Petersen <tobsf@hotmail.com>
2018-09-14 08:46:56 +0200

    responsive

--- sites/all/themes/cb/scripts/cb.js
+++ sites/all/themes/cb/scripts/cb.js
@@ -538,7 +538,7 @@ function isMobile()
               }
               else // default
               {
-                  //cjQuery('#facetapi-facet-search-apiglobal-search-block-nodeog-group-ref').parent().parent().parent().css('left','68%').css('bottom',bottom_placement_value);
+                  jQuery('#facetapi-facet-search-apiglobal-search-block-nodeog-group-ref').parent().parent().parent().css('left','68%').css('bottom',bottom_placement_value);
               }
             break; 
           default:
