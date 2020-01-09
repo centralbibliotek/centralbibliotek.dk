c94b22d4a02f84ab5523ec788742efe7af808be9
memleakz <dev@fynbib.dk>
2020-01-09 14:00:36 +0100

    Add write to followers to the group admin page.

---
+++ sites/all/modules/custom/cbib_oc_hooks/js/multi-select-control.js
@@ -0,0 +1,7 @@
+(function($) {
+    //Add listening for typing in body elements
+    
+    $( document ).ready(function() {
+        $('#edit-target-follower-mails').multiSelect();
+    });
+})(jQuery);
