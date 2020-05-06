a0bf698296476f827473d4d119cdde2e59a10599
memleakz <tobsf@hotmail.com>
2020-05-06 11:12:51 +0200

    small changes.

--- sites/all/modules/custom/cbib_project_pages/templates/views-view--projekt-sider-overview.tpl.php
+++ sites/all/modules/custom/cbib_project_pages/templates/views-view--projekt-sider-overview.tpl.php
@@ -1,12 +1,14 @@
 <?php
 
 ?>
-
+<div class="project_page_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a tempus dui, ac sagittis felis. Mauris tortor velit, egestas eget metus vel, egestas sodales metus. Fusce ullamcorper leo egestas dui fermentum,</div>
 <?php
+
 foreach($view->result as $item) {
 $node = node_load($item->nid);    
 ?>
 <a title="<?php echo $node->title ?>" href="/<?php echo drupal_get_path_alias("node/".$node->nid) ?> " class="project_page_view_block_link" target="_blank">
+
 <div class="project_page_view_block_wrap">
 <div class="project_page_view_block_logo">
 
