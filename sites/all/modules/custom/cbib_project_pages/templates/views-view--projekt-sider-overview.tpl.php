2008d5efadb8f37fe5c47e7b2b834a6ad3d53224
impara <impara1@gmail.com>
2020-09-23 08:22:12 +0200

    href target self

--- sites/all/modules/custom/cbib_project_pages/templates/views-view--projekt-sider-overview.tpl.php
+++ sites/all/modules/custom/cbib_project_pages/templates/views-view--projekt-sider-overview.tpl.php
@@ -7,7 +7,7 @@
 foreach($view->result as $item) {
 $node = node_load($item->nid);    
 ?>
-<a title="<?php echo $node->title ?>" href="/<?php echo drupal_get_path_alias("node/".$node->nid) ?> " class="project_page_view_block_link" target="_blank">
+<a title="<?php echo $node->title ?>" target="_self" href="/<?php echo drupal_get_path_alias("node/".$node->nid) ?> " class="project_page_view_block_link" target="_blank">
 
 <div class="project_page_view_block_wrap">
 <div class="project_page_view_block_logo">
