14e7f02d3c2f837ad67f21e634c4ef39d1283eab
Kent Gottschalk Hansen <imox@pop-os.localdomain>
2020-12-18 19:46:59 +0100

    temaspor styling v8 scroll v3

--- sites/all/themes/cb/scripts/cb.js
+++ sites/all/themes/cb/scripts/cb.js
@@ -573,6 +573,9 @@
   // Temaspor undermenu scroll
   Drupal.behaviors.temascroll = {
     attach: function (context, settings) {
+      $(".view-temaspor-undermenu .views-field-title .field-content").append(
+        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' style='height: 1..5rem; width: 1.5rem; margin-left: 5px;'><path fill-rule='evenodd' d='M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z' clip-rule='evenodd'></path></svg>"
+      );
       $("a[href*=\\#]").on("click", function (event) {
         const hash = this.hash;
         event.preventDefault();
